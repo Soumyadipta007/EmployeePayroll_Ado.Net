1b1155175da55ebd72e072d9b25e1646a52fcaf1
Soumya <soumyadipta0077@gmail.com>
2020-11-05 21:43:41 +0530

    [Soumya] Add: Adding Connection to Database

---
+++ EmployeePayroll_Ado.Net/EmployeePayroll_Ado.Net/EmployeeRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll_Ado.Net
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=DESKTOP-JQ4KIAJ\SQLEXPRESS;Initial Catalog=Payroll_Service_DB;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void checkConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("Connection established");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }        
+    }
+}
+
---
+++ EmployeePayroll_Ado.Net/EmployeePayroll_Ado.Net/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeePayroll_Ado.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll!");
+            EmployeeRepo repo = new EmployeeRepo();
+            repo.checkConnection();
+        }
+    }
+}
